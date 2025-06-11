3a43e8cfbeaac7af2316b4dd267d103fbd0141ee
Martin Lindner <ml17950@gmail.com>
2025-06-11 12:24:02 +0200

    Released v0.1.38

--- publish/make-release.bat
+++ publish/make-release.bat
@@ -10,7 +10,7 @@ xcopy ..\dist\*.* .\Application-Files\dist\ /Y
 
 :createZipArchive
 if exist .\rcman-release.zip del .\rcman-release.zip
-7z.exe a -t7z -mx9 -r .\rcman-release.zip .\Application-Files\*.*
+"%ProgramFiles%\7-Zip\7z.exe" a -t7z -mx9 -r .\rcman-release.zip .\Application-Files\*.*
 
 :createSetup
 "%ProgramFiles(x86)%\Inno Setup 6\ISCC.exe" %~dp0setup.iss
